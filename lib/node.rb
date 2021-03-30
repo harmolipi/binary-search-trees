d39ac4e4f76e964c7f4489de099bf21b49a33b44
harmolipi <nikob381@gmail.com>
2021-03-30 01:27:57 -0400

    included Comparable module to compare nodes

--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,15 @@
 # class to create and manage nodes within the binary search tree
 class Node
+  include Comparable
   attr_accessor :data, :left, :right
 
-  def initialize(data, left = nil, right = nil)
+  def initialize(data)
     @data = data
     @left = left
     @right = right
+  end
+
+  def <=>(other)
+    data <=> other.data
   end
 end
